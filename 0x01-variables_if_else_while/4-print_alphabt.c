7c52ab70e790866e024f58f8af597e85e245d816
salmamina <elfellah.s@gmail.com>
2023-06-17 00:53:53 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabetexcept q and e
+ *
+ *Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	/*print a-z*/
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+			putchar('\n');
+		}
+		ch++;
+	}
+	return (0);
+}

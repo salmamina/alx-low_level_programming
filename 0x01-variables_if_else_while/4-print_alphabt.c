e5a3242f9b4d1c63abfee9fc3887976fc449b2cd
salmamina <elfellah.s@gmail.com>
2023-06-17 00:59:16 +0100

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,11 +14,11 @@ int main(void)
 	while (ch <= 'z')
 	{
 		if (ch != 'q' || ch != 'e')
-		{
-			putchar(ch);
-			putchar('\n');
-		}
+			ch++;
+		putchar(ch);
 		ch++;
 	}
+	putchar('\n');
+
 	return (0);
 }

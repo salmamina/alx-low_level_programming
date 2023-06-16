973bf61dc73fc7c4a9ece0a41fad0e6bc22a9e83
salmamina <elfellah.s@gmail.com>
2023-06-17 00:26:07 +0100

    letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);

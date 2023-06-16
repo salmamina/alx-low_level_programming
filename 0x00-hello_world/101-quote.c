77ac73ecc310fa68d6028ea6ee1b791f23a51054
salmamina <elfellah.s@gmail.com>
2023-06-16 02:35:43 +0100

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - program that print line without using printf or puts line to standard error
- * Return: 1 (Success)
+ * main - program that prints a line without using printf
+ *        or puts, writes the line to standard error
+ *
+ *Return: 1 (Success)
  */
 int main(void)
-{ 
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1,quo,59);
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quo, 59);
 return (1);
 }

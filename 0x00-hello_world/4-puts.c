2c56a42e9169fe2d39a7b295cb8ae4090bffafe6
salmamina <elfellah.s@gmail.com>
2023-06-16 01:07:16 +0100

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - program that prints line with puts function
+ * Return: 0 (if success(
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

5f0d05c74812bdaa2c8c17c61c09216dae1c0e29
salmamina <elfellah.s@gmail.com>
2023-06-16 02:12:32 +0100

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 { 
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1,quo,59);
 return (1);
 }

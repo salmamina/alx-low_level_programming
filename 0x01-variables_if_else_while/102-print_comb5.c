712f76d72c96f062d1b3ff2674d2727b020bc3fe
salmamina <elfellah.s@gmail.com>
2023-06-18 01:31:50 +0100

    ex11

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <studio.h>
+
+/**
+ * main - combinaison two two numbers
+ *
+ * Return: 0 (success)
+*/
+
+int main (void)
+{
+	int fdigit = 0, sdigit;
+
+	while (fdigit <= 99)
+	{
+		sdigit = fdigit;
+		while (sdigit <= 99)
+		{
+			if (sdigit != fdigit)
+			{
+				putchar((fdigit / 10) + 48);
+				putchar((fdigit % 10) + 48);
+				putchar(' ');
+				putchar((sdigit / 10) + 48);
+				putchar((sdigit % 10) + 48);
+				if (fdigit != 98 || sdigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			sdigit++;
+		}
+		fdigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}

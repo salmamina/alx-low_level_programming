d79c6d1c45d26c2a29241afa0ef52fa89a32cacc
salmamina <elfellah.s@gmail.com>
2023-06-20 23:36:30 +0100

    print 10 time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@
 void print_alphabet_x10(void)
 {
 	int i, ch;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
-		for (ch = 'a'; ch == 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}

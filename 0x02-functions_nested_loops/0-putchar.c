20bf9701ecc1834dfff2c73a30bb2cf093c28beb
salmamina <elfellah.s@gmail.com>
2023-06-20 22:15:13 +0100

    _putchar defined

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,8 +12,8 @@ int main(void)
 	char str[] = "_putchar";
 	int i;
 
-	for (i = 0; i < strlen(str); i++)
-		putchar(str[i]);
+	for (i = 0; i < strlen(str)-1 ; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 
 	return (0);

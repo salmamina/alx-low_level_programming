b39504c3efaf667291141c16d23f30023df0cacd
salmamina <elfellah.s@gmail.com>
2023-06-21 15:16:37 +0100

    print numbers under 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that print numbers between n and 98
+ *
+ * @n: number to start
+ *
+*/
+
+void print_to_98(int n)
+{
+	if (n > 98)
+		for (i = n; i >= 98; i--)
+			printf(i, ", ");
+	else
+	{
+		for (i = n; i < 98; i++)
+			printf(i, ", ");
+	}
+	printf("98\n");
+}

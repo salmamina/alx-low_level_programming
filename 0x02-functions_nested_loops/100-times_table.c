534401f3466e9514188e92e71caec6b00f6892ff
salmamina <elfellah.s@gmail.com>
2023-06-21 22:27:28 +0100

    print time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - unction that prints the n times table, starting with 0
+ * @n: number n time
+*/
+
+void print_times_table(int n)
+{
+	int prod, i, j, r;
+
+	if (n >= 0 && n <= 1)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				prod = i * j;
+				_putchar(",");
+				_putchar(" ");
+
+				if prod <= 9
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + 48);
+				}
+				else if prod <= 99
+				{
+					_putchar(' ');
+					_putchar(prod / 10 + 48);
+					_putchat(prod % 10 + 48);
+				}
+				else
+				{
+					_putchar(prod / 100 + 48);
+					r = prod - 100;
+					_putchar(r / 10 + 48);
+					_putchar(((r / 10) % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}

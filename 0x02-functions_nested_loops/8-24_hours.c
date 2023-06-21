dac2914809c62feb56a81f721ceee47b21e0888e
salmamina <elfellah.s@gmail.com>
2023-06-21 12:49:34 +0100

    prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			_putchar(hr + 48);
+			_putchar(hr / 10 + 48);
+			_putchar(hr % 10 + 48);
 			_putchar(':');
-			_putchar(min + 48);
+			_putchar(min / 10 + 48);
+			_putchar(min % 10 + 48);
 			_putchar('\n');
 		}
 	}

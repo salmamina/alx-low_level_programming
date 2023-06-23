f67384b38586e35764ea6a8e28134e4c7d8d782f
SALMA <69902401+salmamina@users.noreply.github.com>
2023-06-23 16:44:47 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,14 +3,10 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 
 #endif /* MAIN_H */

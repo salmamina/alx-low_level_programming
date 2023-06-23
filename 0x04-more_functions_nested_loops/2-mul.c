ad32d3028a8f8c083bff06bbc6418ea6179a49cb
salmamina <elfellah.s@gmail.com>
2023-06-23 15:54:01 +0100

    multiple two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiple twe integer
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: result of multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

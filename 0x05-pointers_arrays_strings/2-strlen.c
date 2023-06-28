65cb1bd58ceef1c4a727e2fff6b5caf41e6d478e
salmamina <elfellah.s@gmail.com>
2023-06-28 11:28:24 +0100

    legth of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: input value
+ * Return: length of string
+ *
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s = '\0', s++)
+		++i;
+	return (i);
+}

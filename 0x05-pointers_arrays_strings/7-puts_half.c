a5d5b32edf9aa06ac4ffc4aee8cf345f509abf14
salmamina <elfellah.s@gmail.com>
2023-06-28 13:02:08 +0100

    print half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,13 @@
 void puts_half(char *str)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (str[i])
 		i++;
-	while (j < i / 2 + 1)
+
+	j = i / 2 + 1;
+	while (j < i)
 	{
 		_putchar(str[j]);
 		j++;

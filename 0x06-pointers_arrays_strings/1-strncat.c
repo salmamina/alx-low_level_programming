8d1a41fde14b91787f85f87230c0b4197744cfd0
salmamina <elfellah.s@gmail.com>
2023-06-30 16:24:49 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,9 +17,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i])
 		i++;
+
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[i + j] = src[j];
 
-	dest[c + i] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }

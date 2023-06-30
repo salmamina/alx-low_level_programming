082794b656e138a53f03f0933a785f72bae7f3e4
salmamina <elfellah.s@gmail.com>
2023-06-30 16:32:05 +0100

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy string in string.
+ *
+ * @dest: first input
+ * @src: second input
+ * @n: numbre of byte
+ *
+ * Return: pointer to the resulting string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n && src[i] != '\n'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}

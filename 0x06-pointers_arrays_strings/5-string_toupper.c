3723e95b7d228e83e9a04bf6b3e6bfeb156ee027
salmamina <elfellah.s@gmail.com>
2023-07-01 01:58:56 +0100

    change lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - change lower to uppercase
+ *
+ * @str: second input
+ * Return: string
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}

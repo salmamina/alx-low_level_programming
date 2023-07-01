388dba204fd1d872ae926c04bc614298d4ac28f2
salmamina <elfellah.s@gmail.com>
2023-07-01 01:16:05 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcmp - compare two strings.
+ *
+ * @s1: first input
+ * @s2: second input
+ * Return: pointer to the resulting string
+*/
+
+char *_strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		i++;
+	}
+	return (equal);
+}

7d4baa2dde1d4fcae76e8a164ca23f629a82df6b
salmamina <elfellah.s@gmail.com>
2023-07-03 15:21:52 +0100

    locate character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locate a caracter in a string
+ *
+ * @s: first input
+ * @c: second first
+ * Return: pointer to first occurrence
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -26,6 +26,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 
 #endif /* MAIN_H */

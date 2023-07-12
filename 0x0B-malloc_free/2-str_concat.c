1e30e054fbbf6aa26064305e7b6eeee7259a3f4f
salmamina <elfellah.s@gmail.com>
2023-07-12 09:50:50 +0100

    str concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * str_concat - function thatreturn pointer to newly allocated memory
+ *
+ * @s1: first input
+ * @s2: second input
+ * Return: pointer of char
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	int i, j, c, d;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (i = 0; s2[i] != '\0'; i++)
+		;
+
+	a = malloc((i * sizeof(*a)) + (j * sizeof(*a)) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
+	}
+	return (a);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -43,5 +43,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif /* MAIN_H */

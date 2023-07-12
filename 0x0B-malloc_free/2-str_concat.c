a6930282e2a5662e61f8a4c4052215435c029a44
salmamina <elfellah.s@gmail.com>
2023-07-12 09:58:13 +0100

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (i = 0; s2[i] != '\0'; i++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	a = malloc((i * sizeof(*a)) + (j * sizeof(*a)) + 1);

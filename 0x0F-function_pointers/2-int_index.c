e68e6847e3e69a7090693423c4f50188f497ee1f
salmamina <elfellah.s@gmail.com>
2023-07-19 19:25:15 +0100

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute function on each elemnt of array
+ * @array: array of element
+ * @size: size of array:
+ * @cmp: function pointer
+ * Return: -1 if no element, 0 otherwise
+*/
+
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int i = 0;
+
+	if (array && action && size)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}

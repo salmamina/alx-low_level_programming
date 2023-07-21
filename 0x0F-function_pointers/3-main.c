c357247844bdf9becaebc2fceae0a9cb8f6269a4
salmamina <elfellah.s@gmail.com>
2023-07-21 09:02:11 +0100

    correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,26 +19,19 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	o = get_op_func(argv[2]);
-	if (o == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+
 	o = get_op_func(argv[2]);
 	if (o == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (!b && (argv[][]
+
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
 	printf("%d\n", o(a, b));
 	return (0);
 }

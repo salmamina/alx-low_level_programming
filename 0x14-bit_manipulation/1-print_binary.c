69f6951b8bb60d26a7ef433ff599a9fa1fdc73ca
salmamina <elfellah.s@gmail.com>
2023-08-04 01:47:00 +0100

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertable char
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
 */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int total, power;
-	int len;
 
-	if (b == NULL)
-		return (0);
-	for (len = 0; b[len]; len++)
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+		printf("0");
+		return;
 	}
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+	for (; shifts >= 0; shifts--)
 	{
-		if (b[len] == '1')
-			total += power;
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
 	}
-	return (total);
 }
